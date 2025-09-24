a52660a0e67fdd73d3dbcb3418aec03fe454a874
Fereshteh Aminikhoo <faminikhoo@gmail.com>
2025-09-24 11:27:15 +0330

    Fix - creating type model, migration and data seeder

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $types = [
+            "full-time",
+            "part-time",
+            "remote",
+            "internship",
+        ];
+
+        foreach ($types as $type){
+            DB::table('job_types')->updateOrInsert(
+                ["name" => $type],
+                ["created_at" => now(), "updated_at" => now()],
+            );
+        }
+    }
+}
